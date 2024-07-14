6ea585b4d0c23d363733ba02338c29871216b920
Maurício Xavier Oliveira <xavierdeoliveiramauricio@gmail.com>
2024-07-14 20:14:37 -0300

    feat: make endpoit `/status` file return more informations

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1");
-  console.log(result);
-  res.status(200).json({ message: "Tudo certo por aqui :)" });
+  const databaseName = process.env.POSTGRES_DB;
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionRes = await database.query("SHOW server_version");
+  const maxConnectionRes = await database.query("SHOW max_connections");
+  const openConnectionRes = await database.query({
+    text: "SELECT count(*)::int AS open_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseVersionValue = databaseVersionRes.rows[0].server_version;
+  const maxConnectionsValue = parseInt(
+    maxConnectionRes.rows[0].max_connections,
+  );
+  const openConnectionsValue = openConnectionRes.rows[0].open_connections;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: maxConnectionsValue,
+        open_connections: openConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,12 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const body = await response.json();
+
+  const regex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
 
   expect(response.status).toBe(200);
+  expect(body.updated_at).toMatch(regex);
+  expect(body.dependecies.database.version).toBe("16.3");
+  expect(body.dependecies.database.max_connections).toBe(100);
+  expect(body.dependecies.database.open_connections).toBeGreaterThanOrEqual(1);
 });
