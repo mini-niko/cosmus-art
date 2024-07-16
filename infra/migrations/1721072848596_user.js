70169ce98bd9b8b9f4c4a329546466820fe5fd5c
Maurício Xavier Oliveira <119255200+mini-niko@users.noreply.github.com>
2024-07-16 20:29:26 +0000

    feat: add `user` migration

---
+++ infra/migrations/1721072848596_user.js
@@ -0,0 +1,49 @@
+const { PgLiteral } = require("node-pg-migrate");
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = async (pgm) => {
+  await pgm.createTable("user", {
+    id: {
+      type: "uuid",
+      default: pgm.func("gen_random_uuid()"),
+      notNull: true,
+      primaryKey: true,
+    },
+    name: {
+      type: "varchar(50)",
+      unique: true,
+      notNull: true,
+    },
+    email: {
+      type: "varchar(254)",
+      unique: true,
+      notNull: true,
+    },
+    password: {
+      type: "varchar(50)",
+      notNull: true,
+    },
+    timestamp: {
+      type: "timestamptz",
+      default: pgm.func("current_timestamp"),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = async (pgm) => {
+  await pgm.dropTable("user");
+};
