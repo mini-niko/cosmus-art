22bf55adbc0e132a2d12c70646b5eeb201c8732a
Maurício Xavier Oliveira <119255200+mini-niko@users.noreply.github.com>
2024-07-27 13:22:55 +0000

    feat: add GET method in `/user` endpoint

--- models/user.js
+++ models/user.js
@@ -1,31 +1,75 @@
 import database from "infra/database.js";
 
 async function getAll() {
-  const responseQuery = await database.query("SELECT * FROM account");
+  const responseQuery = await database.query(
+    "SELECT id, name, timestamp FROM account;",
+  );
 
   return responseQuery.rows;
 }
 
-async function create(user) {
+async function nameExists(name) {
   const responseQuery = await database.query({
-    text: "INSERT INTO account(name, email, password) VALUES($1, $2, $3) RETURNING *",
-    values: [user.name, user.email, user.password],
+    text: "SELECT * FROM account WHERE name = $1;",
+    values: [name],
   });
 
-  console.log(responseQuery.rows);
+  const exists = responseQuery.rows.length > 0 ? true : false;
+
+  return exists;
+}
+
+async function emailExists(email) {
+  const responseQuery = await database.query({
+    text: "SELECT * FROM account WHERE email = $1;",
+    values: [email],
+  });
+
+  const exists = responseQuery.rows.length > 0 ? true : false;
+
+  return exists;
+}
+
+async function getByName(name) {
+  const responseQuery = await database.query({
+    text: "SELECT * FROM account WHERE name = $1;",
+    values: [name],
+  });
+
+  return responseQuery.rows[0] || {};
+}
+
+async function getByEmail(email) {
+  const responseQuery = await database.query({
+    text: "SELECT * FROM account WHERE email = $1;",
+    values: [email],
+  });
+
+  return responseQuery.rows[0] || {};
+}
+
+async function create(user) {
+  const responseQuery = await database.query({
+    text: "INSERT INTO account(name, email, password) VALUES($1, $2, $3) RETURNING *;",
+    values: [user.name, user.email, user.password],
+  });
 
   return responseQuery.rows[0];
 }
 
 async function drop(user) {
   const result = await database.query({
-    text: "WITH deleted AS (DELETE FROM account RETURNING *) SELECT COUNT(*) FROM deleted",
+    text: "WITH deleted AS (DELETE FROM account RETURNING *) SELECT COUNT(*) FROM deleted;",
     query: [user.name, user.email, user.password],
   });
 }
 
 export default {
   getAll,
+  getByName,
+  getByEmail,
+  nameExists,
+  emailExists,
   create,
   drop,
 };
--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -1,19 +1,39 @@
 import user from "models/user";
 
 function index(req, res) {
-  const modelUser = JSON.parse(req.body);
+  return new Promise((resolve, reject) => {
+    const modelUser = req.body ? JSON.parse(req.body) : {};
 
-  const methods = {
-    POST() {
-      user.create(modelUser).then((newUser) => res.status(201).json(newUser));
-    },
-    DELETE() {
-      user.drop(modelUser).then(() => res.status(204).send());
-    },
-  };
+    const methods = {
+      GET() {
+        user.getByName(req.query.name).then((userFound) => {
+          res.status(200).json(userFound);
+          resolve();
+        });
+      },
+      POST() {
+        user.create(modelUser).then((newUser) => {
+          res.status(201).json(newUser);
+        });
 
-  const execute = methods[req.method];
-  execute ? execute() : res.status(405).end();
+        resolve();
+      },
+      DELETE() {
+        user.drop(modelUser).then(() => {
+          res.status(204).send();
+          resolve();
+        });
+      },
+    };
+
+    const execute = methods[req.method];
+    execute
+      ? execute()
+      : (res
+          .status(405)
+          .json({ code: 405, message: "This method is not allowed" }),
+        resolve());
+  });
 }
 
 export default index;
