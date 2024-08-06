a42e62ff5af8f49c89e452335303de9efa0ed1dc
Maurício Xavier Oliveira <119255200+mini-niko@users.noreply.github.com>
2024-08-06 01:00:34 +0000

    refactor: change `infra/database.js` file and `/user` endpoint

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -1,39 +1,33 @@
 import user from "models/user";
 
 function index(req, res) {
-  return new Promise((resolve, reject) => {
-    const modelUser = req.body ? JSON.parse(req.body) : {};
+  const modelUser = req.body ? JSON.parse(req.body) : {};
 
-    const methods = {
-      GET() {
-        user.getByName(req.query.name).then((userFound) => {
-          res.status(200).json(userFound);
-          resolve();
-        });
-      },
-      POST() {
-        user.create(modelUser).then((newUser) => {
-          res.status(201).json(newUser);
-        });
+  const methods = {
+    GET() {
+      user.getByName(req.query.name).then((userFound) => {
+        return res.status(200).json(userFound);
+      });
+    },
+    POST() {
+      user.create(modelUser).then((newUser) => {
+        return res.status(201).json(newUser);
+      });
+    },
+    DELETE() {
+      user.drop(modelUser).then(() => {
+        return res.status(204).send()
+      });
+    },
+  };
 
-        resolve();
-      },
-      DELETE() {
-        user.drop(modelUser).then(() => {
-          res.status(204).send();
-          resolve();
-        });
-      },
-    };
-
-    const execute = methods[req.method];
-    execute
-      ? execute()
-      : (res
-          .status(405)
-          .json({ code: 405, message: "This method is not allowed" }),
-        resolve());
-  });
+  const execute = methods[req.method];
+  return execute
+    ? execute()
+    : (res
+        .status(405)
+        .json({ error: `Method "${request.method}" not allowed` }),
+      );
 }
 
 export default index;
