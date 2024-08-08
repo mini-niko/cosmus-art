d0884eef6d09b71a710c21957c4d4a375d58156b
mini-niko <xavierdeoliveiramauricio@gmail.com>
2024-08-07 21:38:38 -0300

    tests: add more tests for `/user` endpoint

--- tests/integration/api/v1/user/create.test.js
+++ tests/integration/api/v1/user/create.test.js
@@ -20,10 +20,31 @@ test("POST to /api/v1/user should return 201", async () => {
     "application/json",
   );
 
+  const requestBody = JSON.stringify(modelUser);
+
+  const response = await fetch("http://localhost:3000/api/v1/user", {
+    headers: requestHeaders,
+    method: "POST",
+    body: requestBody,
+  });
+
+  const responseBody = await response.json();
+
+  expect(response.status).toBe(201);
+  expect(responseBody.name).toBe(modelUser.name);
+  expect(responseBody.email).toBe(modelUser.email);
+  expect(responseBody.password).toBe(modelUser.password);
+});
+
+test("POST to /api/v1/user with existing name should return 409", async () => {
+  const requestHeaders = new Headers().append(
+    "Content-Type",
+    "application/json",
+  );
+
   const requestUser = {
-    name: "test_cosmus",
-    email: "email@example.com",
-    password: "@cosmusart",
+    ...modelUser,
+    email: "email2@example.com",
   };
 
   const requestBody = JSON.stringify(requestUser);
@@ -36,8 +57,37 @@ test("POST to /api/v1/user should return 201", async () => {
 
   const responseBody = await response.json();
 
-  expect(response.status).toBe(201);
-  expect(responseBody.name).not.toBeUndefined();
-  expect(responseBody.email).not.toBeUndefined();
-  expect(responseBody.password).not.toBeUndefined();
+  expect(response.status).toBe(409);
+  expect(responseBody).not.toBeUndefined();
+  expect(responseBody.error).toBe(
+    "Unable to create a user with an existing name.",
+  );
+});
+
+test("POST to /api/v1/user with existing email should return 409", async () => {
+  const requestHeaders = new Headers().append(
+    "Content-Type",
+    "application/json",
+  );
+
+  const requestUser = {
+    ...modelUser,
+    name: "test2_cosmus",
+  };
+
+  const requestBody = JSON.stringify(requestUser);
+
+  const response = await fetch("http://localhost:3000/api/v1/user", {
+    headers: requestHeaders,
+    method: "POST",
+    body: requestBody,
+  });
+
+  const responseBody = await response.json();
+
+  expect(response.status).toBe(409);
+  expect(responseBody).not.toBeUndefined();
+  expect(responseBody.error).toBe(
+    "Unable to create a user with an existing email.",
+  );
 });
--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -1,3 +1,4 @@
+import migrations from "models/migrations";
 import user from "models/user";
 
 const modelUser = {
@@ -24,7 +25,7 @@ test("GET to /api/v1/user/get-all should return 200", async () => {
   expect(Array.isArray(users)).toBeTruthy();
 });
 
-test("GET to /api/v1/user with name parameter should return 200", async () => {
+test("GET to /api/v1/user with valid name parameter should return 200", async () => {
   const requestUrl = new URL("http://localhost:3000/api/v1/user/");
   requestUrl.searchParams.set("name", "test_cosmus");
 
@@ -39,5 +40,43 @@ test("GET to /api/v1/user with name parameter should return 200", async () => {
   });
 
   const body = await response.json();
+  const user = body[0];
+
+  expect(body.length).toBe(1);
+  expect(typeof user.id).toBe("string");
+  expect(user.id).toMatch(
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/,
+  );
+  expect(typeof user.name).toBe("string");
+  expect(user.name).toBe("test_cosmus");
+  expect(typeof user.email).toBe("string");
+  expect(user.email).toBe("email@example.com");
+  expect(typeof user.password).toBe("string");
+  expect(user.password).toBe("@cosmusart");
+  expect(typeof user.timestamp).toBe("string");
+  expect(user.timestamp).toMatch(
+    /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2})$/,
+  );
 });
-//
+
+test("GET to /api/v1/user with invalid name parameter should return 404", async () => {
+  const requestUrl = new URL("http://localhost:3000/api/v1/user/");
+  requestUrl.searchParams.set("name", "invalid_name_cosmus");
+
+  const requestHeaders = new Headers().append(
+    "Content-Type",
+    "application/json",
+  );
+
+  const response = await fetch(requestUrl.href, {
+    headers: requestHeaders,
+    method: "GET",
+  });
+
+  const body = await response.json();
+
+  expect(response.status).toBe(404);
+
+  expect(body).not.toBeUndefined();
+  expect(body.error).toBe("This user doesn't exists.");
+});
