f13aae5782b36428d3ab4979b7e50b8a7f578dbc
mini-niko <xavierdeoliveiramauricio@gmail.com>
2024-08-09 17:18:38 -0300

    chore: change `/user/get-all` endpoint body response

--- pages/api/v1/user/get-all.js
+++ pages/api/v1/user/get-all.js
@@ -3,9 +3,7 @@ import user from "models/user";
 async function getAll(req, res) {
   const allUsers = await user.getAll();
 
-  res.status(200).json({
-    users: allUsers,
-  });
+  res.status(200).json(allUsers);
 }
 
 export default getAll;
